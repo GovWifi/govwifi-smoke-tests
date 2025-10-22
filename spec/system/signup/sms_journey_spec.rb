1a5bfd7288b7900984d4dca35f0a96285ea0fd6f
leonaAtkins <leona.atkins@digital.cabinet-office.gov.uk>
2025-10-22 16:44:02 +0100

    re-add sms tests

--- spec/system/signup/sms_journey_spec.rb
+++ spec/system/signup/sms_journey_spec.rb
@@ -2,22 +2,22 @@ require "notifications/client"
 require_relative "../../../lib/notify_sms"
 
 # Don't run this test in production as it creates false red flags due to Notify slow responses
-# feature "SMS Journey", if: ENV["ENVIRONMENT"] != "production" do
-#   include NotifySms
-#   before :each do
-#     remove_user(user: ENV["SMOKETEST_PHONE_NUMBER"])
-#   end
-#   it "signs up using an SMS journey" do
-#     id = get_first_sms(phone_number: ENV["GOVWIFI_PHONE_NUMBER"])&.id
-#     send_go_message(phone_number: ENV["GOVWIFI_PHONE_NUMBER"], template_id: ENV["NOTIFY_GO_TEMPLATE_ID"])
-#     message = read_reply_sms(phone_number: ENV["GOVWIFI_PHONE_NUMBER"], after_id: id)
-#     username, password = parse_sms_message(message:)
+feature "SMS Journey", if: ENV["ENVIRONMENT"] != "production" do
+  include NotifySms
+  before :each do
+    remove_user(user: ENV["SMOKETEST_PHONE_NUMBER"])
+  end
+  it "signs up using an SMS journey" do
+    id = get_first_sms(phone_number: ENV["GOVWIFI_PHONE_NUMBER"])&.id
+    send_go_message(phone_number: ENV["GOVWIFI_PHONE_NUMBER"], template_id: ENV["NOTIFY_GO_TEMPLATE_ID"])
+    message = read_reply_sms(phone_number: ENV["GOVWIFI_PHONE_NUMBER"], after_id: id)
+    username, password = parse_sms_message(message:)
 
-#     eapol_test = GovwifiEapoltest.new(radius_ips: ENV["RADIUS_IPS"].split(","),
-#                                       secret: ENV["RADIUS_KEY"])
+    eapol_test = GovwifiEapoltest.new(radius_ips: ENV["RADIUS_IPS"].split(","),
+                                      secret: ENV["RADIUS_KEY"])
 
-#     output = eapol_test.run_peap_mschapv2(username:,
-#                                           password:)
-#     expect(output).to all(have_been_successful), output.join("\n")
-#   end
-# end
+    output = eapol_test.run_peap_mschapv2(username:,
+                                          password:)
+    expect(output).to all(have_been_successful), output.join("\n")
+  end
+end
